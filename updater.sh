2696f89ce4e6ca3409c9b6c881e62f1e32f12b1b
avarento <siede@protonmail.com>
2023-03-02 01:27:20 -0300

    solução updater

--- updater.sh
+++ updater.sh
@@ -10,12 +10,14 @@ echo "(3/7) - Node atualizado"
 npm update
 echo "(4/7) - NPM list atualizado"
 
+git pull origen master
+echo "(5/7) - Repositório atualizado"
+
 git add registro.db usuarios.db
 git commit -m "update Registro e Usuarios" && git push -u origen master
-echo "(5/7) - Banco de dados salvo"
+echo "(6/7) - Banco de dados salvo"
 
-git pull origen master
-echo "(6/7) - Repositório atualizado"
+
 
 pm2 start index
 echo "(7/7) - Processo iniciado"
